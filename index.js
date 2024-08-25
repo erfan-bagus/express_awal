c82c3ad177f5950e8ed2fd798f850b2ed2192b57
erfan_bagus <erfanbagus@hotmail.com>
2024-08-25 20:23:10 +0700

    update project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express  = require('express');
 const app = express();
 const cors = require('cors');
+const PORT  = process.env.PORT || 3000;
+
+app.use(cors())
 app.get('/', (req, res) => {
     // console.log(req.headers.cookie)
     res.send('Hello World!')
 })
 
-app.listen(8000,()=>{
-    console.log("🔥 Listen Server PORT: \x1b[33m 8000 \x1b[0m 🔥")
+app.listen(PORT,()=>{
+    console.log(`🔥 Listen Server PORT: \x1b[33m ${PORT} \x1b[0m 🔥`)
 })
